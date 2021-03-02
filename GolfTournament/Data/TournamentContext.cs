b688f6894ed7834c5f081d52dbf9fe06d366c75c
Tyler <bisig1892@gmail.com>
2021-03-01 22:58:35 -0800

    Set up DBContext

---
+++ GolfTournament/Data/TournamentContext.cs
@@ -0,0 +1,18 @@
+﻿using GolfTournament.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfTournament.Data
+{
+    public class TournamentContext : DbContext
+    {
+        public TournamentContext(DbContextOptions<TournamentContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tournament> Tournaments { get; set; }
+    }
+}
