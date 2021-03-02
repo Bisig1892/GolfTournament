3620fb697597525d86785d853eae3a36bd200937
Tyler <bisig1892@gmail.com>
2021-03-01 22:54:00 -0800

    Created Tournament Model

---
+++ GolfTournament/Models/Tournament.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfTournament.Models
+{
+    public class Tournament
+    {
+        public int TournamentId { get; set; }
+        /// <summary>
+        /// Id of the course that is being used for the tournament
+        /// </summary>
+        public int CourseId { get; set; }
+        /// <summary>
+        /// Date the tournament is being played 
+        /// </summary>
+        public DateTime ScheduledDate { get; set; }
+    }
+}
