c6174608b770f2841acc5ea77f0f5445f7d443cd
Tyler <bisig1892@gmail.com>
2021-03-02 00:00:32 -0800

    Added Course Model

---
+++ GolfTournament/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfTournament.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        /// <summary>
+        /// Name of the course
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Total Par for whole course
+        /// </summary>
+        public int Par { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZIP { get; set; }
+    }
+}
